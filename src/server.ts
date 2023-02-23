9577f3338b5d97b9a482bff93b3cd7c9fedf679c
Mayur Upadhayay <mayur123a@gmail.com>
2023-02-24 00:15:39 +0530

    feat: Created a minimalistic TCP server

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+/**
+ * Creates a TCP Socket server
+ */
+// common module imports
+import { Server, createServer, Socket } from "net";
+
+const TCP_PORT = 8124;
+
+// Create an instance of TCP socket server
+const server: Server = createServer((socket: Socket): void => {
+    console.log(socket);
+});
+
+// Error handler on server
+server.on('error', (error: any): void => {
+    console.log(error);
+});
+
+server.listen(TCP_PORT, () => {
+    console.log(`TCP Server listening on ${TCP_PORT}`)
+});
